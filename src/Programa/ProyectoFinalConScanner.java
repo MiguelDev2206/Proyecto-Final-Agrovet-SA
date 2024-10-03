648efe3db4c195b585241cf801f2f671787c741c
Miguel_Dev2206 <92831039+MiguelDev2206@users.noreply.github.com>
2024-10-02 22:27:03 -0500

    Proyecto Agrovet V1.0 Main
    Codigo Principal de Proyecto Agrovet

---
+++ src/Programa/ProyectoFinalConScanner.java
@@ -0,0 +1,22 @@
+package Programa;
+
+public class ProyectoFinalConScanner {
+
+    public static void main(String[] args) {
+        // Entradas: Productos iniciales, precios y stock
+        String[] productos = {"Medicinas", "Vitaminas", "Alimentos"}; //categorias de productos
+        String[] nombresProductos = {"Antibiótico", "Vitamina C", "Croquetas"}; //productos
+        int[] stock = {50, 30, 100}; //stock principal por producto
+        double[] precios = {25.5, 15.0, 5.75}; // Precios de los productos
+        int[] ventasDiarias = new int[productos.length]; // ventas diarias por producto
+        int[] ventasMensuales = new int[productos.length]; // ventas mensuales por producto
+
+        int diaActual = 0; // dia en el que se esta trabajando, se inicia en el dia 1
+
+        // Proceso
+
+
+        // Salida
+    }
+}
+
